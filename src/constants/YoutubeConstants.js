ac0e816f82fedda4cb9e65e37daf19a5959b4f17
Spark it <spark-it@Mahdis-MacBook-Pro.local>
2020-11-06 18:46:35 +0100

    Show youtube videos in home page

---
+++ src/constants/YoutubeConstants.js
@@ -0,0 +1,2 @@
+export const MAX_RESULTS = 30;
+export const NUMBER_OF_DAYS = 30;
--- src/screens/Feelings.js
+++ src/screens/Feelings.js
@@ -1,20 +1,14 @@
 import React, {useContext} from 'react';
-import {View} from 'react-native';
 import DefaultText from '../components/shared/DefaultText';
+import Container from '../components/shared/Container';
 import {ThemeContext} from '../utils/Themes';
 
 const Feelings = () => {
-  const {theme} = useContext(ThemeContext);
+  const {changeTheme} = useContext(ThemeContext);
   return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: theme.colors.background,
-      }}>
-      <DefaultText>Feelings Screen</DefaultText>
-    </View>
+    <Container>
+      <DefaultText onPress={changeTheme}>Feelings Screen</DefaultText>
+    </Container>
   );
 };
 
